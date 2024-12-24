89d173b5222465a37397cf8dc74668f5b6215de0
oeggy03 <hathu2516@gmail.com>
2024-12-25 01:57:35 +0800

    Implement authcontext

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ScrollToTop from './components/util/ScrollToTop.js'
+import { AuthProvider } from './components/context/authContext.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <ScrollToTop/>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
